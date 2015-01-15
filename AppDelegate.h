85bcfaa58430a2646acf15e3578cd62b3d0a210d
Kai Aichholz <kai.aichholz@gmail.com>
2015-01-14 16:45:37 -0800

    start commit

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PicRank
+//
+//  Created by Kai Aichholz on 1/14/15.
+//  Copyright (c) 2015 JOKA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
